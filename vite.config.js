574ef8968b51164c2158d9f424d01c3e93cf7533
m.goncharova <m.goncharova@facct.ru>
2024-07-12 19:00:38 +0300

    chore(config): rerun jobs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 import eslint from 'vite-plugin-eslint';
 
+
 export default defineConfig({
     root: resolve(__dirname, 'src'),
     build: {
