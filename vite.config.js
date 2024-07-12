27db108384ffc6130f616b9e594c47fa232ec375
m.goncharova <m.goncharova@facct.ru>
2024-07-12 18:50:46 +0300

    fix(config): typo fix

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
     root: resolve(__dirname, 'src'),
-    publicDir: resolve(__dirname, 'static'),
     build: {
         outDir: resolve(__dirname, 'dist'),
+        emptyOutDir: true,
         rollupOptions: {
             input: {
-                index: resolve(__dirname, './index.html'),
+                index: resolve(__dirname, './src/index.html'),
                 signIn: resolve(
                     __dirname,
                     './src/pages/signIn/SignInPage.html'
