17176457e6218aa519cb0fd1fb0e1475d1691980
Margo <m.goncharova@group-ib.com>
2024-08-14 21:53:57 +0300

    feat(test): Test fix

--- src/shared/stores/Store.ts
+++ src/shared/stores/Store.ts
@@ -6,18 +6,8 @@ export enum StoreEvents {
     chatsUpdated = 'chatsUpdated'
 }
 
-// наследуем Store от EventBus, чтобы его методы были сразу доступны у экземпляра Store
 class Store extends EventBus {
     state: TObject = {};
-    // user;
-    // chatsPreview;
-    // chat:{
-    //     id: {
-    //         lastMessage,
-    //         messages,
-    //         users
-    //     }
-    // }
 
     public getState() {
         return this.state;
@@ -25,7 +15,6 @@ class Store extends EventBus {
     public set(path: string, value: unknown) {
         this.state = set(this.state, path, value) as TObject;
 
-        // метод EventBus
         this.emit(StoreEvents.Updated);
     }
 }
